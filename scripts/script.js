b3a2b15ddc8c0ad0155086082922373c24fcccc5
Natalie Moran <nataliemoran89@live.co.uk>
2023-10-05 17:25:38 +0100

    amend classes

--- scripts/script.js
+++ scripts/script.js
@@ -4,27 +4,6 @@ const rowInputField = document.getElementById("row");
 const percentageInputField = document.getElementById("percent");
 const gridContainer = document.getElementById("container");
 
-const chooseMines = (numOfTiles, percentage) => {
-    const numOfMines = Math.ceil(numOfTiles * percentage);
-    const chosenMines = new Set();
-
-    while (chosenMines.size < numOfMines) {
-        chosenMines.add(Math.ceil(Math.random() * numOfTiles));
-    }
-    return chosenMines;
-};
-
-class Tile {
-    constructor(num, mine, row, col) {
-        this._name = "tile" + (num);
-        this._mine = mine;
-        this._adjacentMines = 0;
-        this._row = row;
-        this._col = col;
-        // future properties - flagged? exploded? exposed/clicked?
-    }
-};
-
 class Grid {
     constructor(rows, columns, rowHeight, columnWidth) {
         this._rows = rows;
@@ -33,8 +12,7 @@ class Grid {
         this._columnWidth = columnWidth; 
         this._totalTiles = rows * columns;   
     }
-
-    createGrid(container) {
+    addToContainer(container) {
         container.style.gridTemplateRows = `repeat(${this._rows}, ${this._rowHeight})`;    
         container.style.gridTemplateColumns = `repeat(${this._columns}, ${this._columnWidth})`; 
 
@@ -51,21 +29,48 @@ class Grid {
         this._divIdArray = divIdArray;
         container.innerHTML = html;
     }
+}
 
-    generateMinesweeperTiles(minePercentage) {
+class MinesweeperTile {
+    constructor(num, mine, row, col) {
+        this._name = "tile" + (num);
+        this._mine = mine;
+        this._adjacentMines = 0;
+        this._row = row;
+        this._col = col;
+        // future properties - flagged? exploded? exposed/clicked?
+    }
+};
+
+class MinesweeperGrid extends Grid {
+    constructor(rows, columns, rowHeight, columnWidth, minePercentage) {
+        super(rows, columns, rowHeight, columnWidth);
+            this._minePercentage = minePercentage;
+    }
+
+    chooseMines() {
+        const numOfMines = Math.ceil(this._totalTiles * this._minePercentage);
+        const chosenMineNums = new Set();
+    
+        while (chosenMineNums.size < numOfMines) {
+            chosenMineNums.add(Math.ceil(Math.random() * this._totalTiles));
+        }
+        this._chosenMineNums = chosenMineNums;
+    };
+
+    generateTileObjects() {
         const tileArray = [];
         const mineArray = [];     
         const nonMineArray = [];
-        const mineSet = chooseMines(this._totalTiles, minePercentage);
-        
+        this.chooseMines();
         let tileNum = 1;
         while (tileNum < this._totalTiles) {
             for (let rowNum = 1 ; rowNum <= this._rows ; rowNum++) {
                 const currentRow = [];
                 for (let colNum = 1; colNum <= this._columns ; colNum++) {
-                    const hasMine = mineSet.has(tileNum);
-                    const newTile = new Tile(tileNum, hasMine, rowNum, colNum);
-
+                    const hasMine = this._chosenMineNums.has(tileNum);
+                    const newTile = new MinesweeperTile(tileNum, hasMine, rowNum, colNum);
+                    newTile._divId = this._divIdArray[tileNum - 1];
                     if (hasMine) {
                         mineArray.push(newTile);
                     }
@@ -78,119 +83,72 @@ class Grid {
                 tileArray.push(currentRow);
             }
         }
-        for (const mine of mineArray) {
-            markAdjacentTiles(mine, tileArray);
-        }
         this._tileArray = tileArray;
         this._mineArray =  mineArray;
         this._nonMineArray = nonMineArray;
-        
-        return tileArray;
+
+        for (const mine of mineArray) {
+            this.amendTiles((tile) => {tile._adjacentMines += 1}, this.findAdjacentTiles(mine));
+        }
+        //return tileArray;
     }
-// integrate into tile creation
-    linkDivsToTiles() {
-        i = 0
-        for (const row of this._tileArray) {
-            for (const tile of row) {
-                tile._divId = this._divIdArray[i];
-                i++
+
+    findAdjacentTiles(tile) {
+        const adjacentTiles = [];
+
+        if (this._tileArray[tile._row]) {
+            adjacentTiles.push(this._tileArray[tile._row][tile._col - 1]);
+        }
+        if (this._tileArray[tile._row - 2]) {
+            adjacentTiles.push(this._tileArray[tile._row - 2][tile._col - 1]);
+        }
+
+        if (this._tileArray[tile._row - 1][tile._col - 2]){
+            adjacentTiles.push(this._tileArray[tile._row - 1][tile._col - 2]);
+            if (this._tileArray[tile._row - 2]) {
+                adjacentTiles.push(this._tileArray[tile._row - 2][tile._col - 2]);
+            }
+            if (this._tileArray[tile._row]) {
+                adjacentTiles.push(this._tileArray[tile._row][tile._col - 2]);
+            }
+
+        }
+        if (this._tileArray[tile._row - 1][tile._col]){
+            adjacentTiles.push(this._tileArray[tile._row - 1][tile._col]);
+            if (this._tileArray[tile._row - 2]) {
+                adjacentTiles.push(this._tileArray[tile._row - 2][tile._col]);
+            }
+            if (this._tileArray[tile._row]) {
+                adjacentTiles.push(this._tileArray[tile._row][tile._col]);
             }
         }
+        return adjacentTiles;
     }
-    
-    
-    /*
-    
-    {
 
-        //dont nest, do simultaneously?
-        const flatTileArray = this._tileArray.flat(); 
-        for (let i = 0 ; i < this._divIdArray.length ; i++) {
-            this._tileArray
-
-            flatTileArray[i]._divId = flatTileArray[i]
-        }
-
-        for (const row of this._tileArray) {
-
-            for (const tile of row) {
-                for (let i = 0 ; i < this._divIdArray.length ; i++){
-                    for (const tile of row) {
-                        tile._divId = this._divIdArray[i];  
-                    }
-                
-                } 
-            }
-        }
-    }
-    */
-    addContentToTiles(contentToAdd, tileArray) {
+    amendTiles(func, tileArray) {
         for (const tile of tileArray) {
-            document.getElementById(tile._divId).innerHTML += contentToAdd;
+            func(tile);
         }
     }
 }
 
-
-
-
-
-
-
-
-
-const markAdjacentTiles = (mine, tileArray) => {
-    if (tileArray[mine._row - 1][mine._col - 2]){
-        tileArray[mine._row - 1][mine._col - 2]._adjacentMines += 1;
-    }
-    if (tileArray[mine._row - 1][mine._col]){
-        tileArray[mine._row - 1][mine._col]._adjacentMines += 1;
-    }
-
-    if (tileArray[mine._row - 2]) {
-        if (tileArray[mine._row - 2][mine._col - 1]) {
-            tileArray[mine._row - 2][mine._col - 1]._adjacentMines += 1;
-        }
-        if (tileArray[mine._row - 2][mine._col - 2]) {
-            tileArray[mine._row - 2][mine._col - 2]._adjacentMines += 1;
-        }
-        if (tileArray[mine._row - 2][mine._col]) {
-            tileArray[mine._row - 2][mine._col]._adjacentMines += 1;
-        }
-    }
-
-    if (tileArray[mine._row]) {
-        if (tileArray[mine._row][mine._col - 1]) {
-            tileArray[mine._row][mine._col - 1]._adjacentMines += 1;
-        }
-        if (tileArray[mine._row][mine._col - 2]) {
-            tileArray[mine._row][mine._col - 2]._adjacentMines += 1;
-        }
-        if (tileArray[mine._row][mine._col]) {
-            tileArray[mine._row][mine._col]._adjacentMines += 1;
-        }
-    }
-};
-
-
-
 const generateFromInput = () => {
     let columnInput = columnInputField.value; 
     let rowInput = rowInputField.value;
-    let tilesTotal = columnInput * rowInput;
     let percentageInput = percentageInputField.value / 100;
 
-    const newGrid = new Grid(rowInput, columnInput, "30px", "30px");
-    newGrid.createGrid(gridContainer);
-    newGrid.generateMinesweeperTiles(percentageInput);
-    newGrid.linkDivsToTiles();
-    newGrid.addContentToTiles("X", newGrid._mineArray);
-    for (const tile of newGrid._nonMineArray) {
+    const minesweeperGrid = new MinesweeperGrid(rowInput, columnInput, "30px", "30px", percentageInput);
+    minesweeperGrid.addToContainer(gridContainer);
+    minesweeperGrid.generateTileObjects();
+    minesweeperGrid.amendTiles((tile) => {document.getElementById(tile._divId).innerHTML = "X"}, minesweeperGrid._mineArray);
+
+    for (const tile of minesweeperGrid._nonMineArray) {
         if (tile._adjacentMines) {
-            newGrid.addContentToTiles(tile._adjacentMines, [tile]);
+            minesweeperGrid.amendTiles((tile) => {document.getElementById(tile._divId).innerHTML = tile._adjacentMines}, [tile]);
         }
     }
 };
 
-
 submitButton.addEventListener("click", generateFromInput);
+
+        
